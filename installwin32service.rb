5a5b0579494c90bfde79cc59fd47f2ebe7725744
Jason Turner <jason@emptycrate.com>
2016-01-12 15:37:43 -0700

    Updates for running service on windows

--- installwin32service.rb
+++ installwin32service.rb
@@ -1,20 +1,7 @@
-require 'win32/service'
 
 
-
-
-# Configure everything
-Service.configure(
-		:service_name       => 'decent_ci',
-		:service_type       => Service::WIN32_OWN_PROCESS,
-		:start_type         => Service::AUTO_START,
-		:error_control      => Service::ERROR_NORMAL,
-		:binary_path_name   => "c:\\Program Files\\Git\\git-bash.exe #{ENV["USERPROFILE"]}\\decent_ci_run.sh #{ENV["USERPROFILE"]}\\decent_ci_config.yaml",
-#		:load_order_group   => 'Network',
-		:service_start_name => "#{ENV["USERDOMAIN"]}\\#{ENV["USERNAME"]}",
-#		:password           => 'XXXXXXX',
-		:display_name       => 'decent_ci build system',
-		:description        => 'A service for running decent_ci on boot'
-		)
+puts(`mkdir c:\\decent_ci_runner`)
+puts(`copy decent_ci_run.sh c:\\decent_ci_runner\\decent_ci_run.sh`)
+puts(`elevate nssm install decent_ci_runner "c:\\Program Files\\Git\\bin\\bash.exe" "c:\\decent_ci_runner\\decent_ci_run.sh" "c:\\decent_ci_runner\\decent_ci_config.yaml" false`)
 
 

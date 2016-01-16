e802cdc9cb528ba824f0534e63ffe47aafd94986
Jason Turner <jason@emptycrate.com>
2016-01-16 08:57:44 -0700

    Revert change to num params to win32service

--- installwin32service.rb
+++ installwin32service.rb
@@ -2,6 +2,6 @@
 
 puts(`mkdir c:\\decent_ci_runner`)
 puts(`copy decent_ci_run.sh c:\\decent_ci_runner\\decent_ci_run.sh`)
-puts(`elevate nssm install decent_ci_runner "c:\\Program Files\\Git\\bin\\bash.exe" "c:\\decent_ci_runner\\decent_ci_run.sh" "c:\\decent_ci_runner\\decent_ci_config.yaml" false false`)
+puts(`elevate nssm install decent_ci_runner "c:\\Program Files\\Git\\bin\\bash.exe" "c:\\decent_ci_runner\\decent_ci_run.sh" "c:\\decent_ci_runner\\decent_ci_config.yaml" false`)
 
 

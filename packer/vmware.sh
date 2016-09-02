7964f18943bad9eb22a5e5f476fc8544c06e8b36
Jason Turner <jason@emptycrate.com>
2016-09-02 14:07:49 -0600

    Add --yes to install of vmware tools

--- packer/vmware.sh
+++ packer/vmware.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 echo "==> Installing VMware Tools"
-sudo apt-get install open-vm-tools-desktop
+sudo apt-get --yes install open-vm-tools-desktop
 rm /home/${SSH_USERNAME}/*.iso
 
 echo "==> Installed VMware Tools"

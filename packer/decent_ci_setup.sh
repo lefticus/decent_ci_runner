a2d7a821f8d54439f680c1da26721bd8d0848304
Jason Turner <jason@emptycrate.com>
2016-09-02 14:39:57 -0600

    Install gnome-session-flashback instead of unity

--- packer/decent_ci_setup.sh
+++ packer/decent_ci_setup.sh
@@ -5,7 +5,7 @@ echo ":ssl_verify_mode: 0" > /home/$SSH_USERNAME/.gemrc
 chown $SSH_USERNAME.$SSH_USERNAME /home/$SSH_USERNAME/.gemrc
 apt-get update
 apt-get -y upgrade
-apt-get -y --no-install-recommends install ubuntu-desktop curl gnome-terminal indicator-session unity-lens-applications
+apt-get -y --no-install-recommends install gnome-session-flashback curl gnome-terminal
 bash <(wget -O - https://raw.githubusercontent.com/lefticus/decent_ci_runner/security_enhancements/setup_ci.sh) true true false
 bash <(wget -O - https://raw.githubusercontent.com/lefticus/decent_ci_runner/security_enhancements/setup_ci.sh) true true false
 apt-get clean

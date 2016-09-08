8e5d98fa176c5d76cf5a2dac933356289b35aaf3
Jason Turner <jason@emptycrate.com>
2016-09-08 14:04:02 -0600

    Install NSSM during bootstrap (win)

--- windows/bootstrap.sh
+++ windows/bootstrap.sh
@@ -36,6 +36,15 @@ else
   HAS_RUBY=0
 fi
 
+command -v nssm
+
+if [ $? -eq 0 ]
+then
+  HAS_NSSM=1
+else
+  HAS_NSSM=0
+fi
+
 
 command -v elevate
 
@@ -85,6 +94,11 @@ then
     echo "You must run this process from an administrative bash console to install 'elevate' and 'ruby'"
     exit 2
   fi
+fi
+
+if [ $HAS_NSSM -eq 0 ]
+then
+    /usr/bin/echo -e "\n\n\n\n" | $ALLUSERSPROFILE\\chocolatey\\bin\\choco install --yes --acceptlicense NSSM
 fi
 
 gem install deep_merge --source http://rubygems.org

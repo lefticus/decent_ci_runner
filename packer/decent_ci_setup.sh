ab4941c90e01219c3485500d400f307b548436b2
Jason Turner <jason@emptycrate.com>
2016-12-16 14:34:02 -0700

    Basic windows bootstrapping with chocolatey

--- packer/decent_ci_setup.sh
+++ packer/decent_ci_setup.sh
@@ -6,8 +6,8 @@ chown $SSH_USERNAME.$SSH_USERNAME /home/$SSH_USERNAME/.gemrc
 apt-get update
 apt-get -y upgrade
 apt-get -y --no-install-recommends install lubuntu-desktop curl gnome-terminal
-bash <(wget -O - https://raw.githubusercontent.com/lefticus/decent_ci_runner/security_enhancements/setup_ci.sh) true true false
-bash <(wget -O - https://raw.githubusercontent.com/lefticus/decent_ci_runner/security_enhancements/setup_ci.sh) true true false
+bash <(wget -O - https://raw.githubusercontent.com/lefticus/decent_ci_runner/add_windows_packer/setup_ci.sh) true true false
+bash <(wget -O - https://raw.githubusercontent.com/lefticus/decent_ci_runner/add_windows_packer/setup_ci.sh) true true false
 apt-get -f -y install
 apt-get clean
 cp /tmp/decent_ci_config.yaml /usr/local/etc/decent_ci_config.yaml

e2a36993f3b0cd3d105106e7c83a1e47e3147949
Jason Turner <jason@emptycrate.com>
2017-01-04 20:50:48 -0700

    Don't require qt - it's installed by OS build

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -3,17 +3,17 @@
 echo "$0 installdeps: '$1'"
 
 
-command -v qmake
-
-if [ $? -eq 0 ]
-then
-  echo "qt is installed"
-else
-  echo "******************************************************"
-  echo "You must install Qt and add its binaries to your path!"
-  echo "******************************************************"
-  exit 1
-fi
+#command -v qmake
+#
+#if [ $? -eq 0 ]
+#then
+#  echo "qt is installed"
+#else
+#  echo "******************************************************"
+#  echo "You must install Qt and add its binaries to your path!"
+#  echo "******************************************************"
+#  exit 1
+#fi
 
 
 
